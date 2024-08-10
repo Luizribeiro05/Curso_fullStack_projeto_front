11932efcff57b81dcf8b3843f173f90d626aa6fc
Luizribeiro05 <112145713+Luizribeiro05@users.noreply.github.com>
2024-08-10 16:27:04 -0300

    clienteList / form

--- gerenciador_de_proutos/src/App.jsx
+++ gerenciador_de_proutos/src/App.jsx
@@ -6,22 +6,26 @@ import FornecedorForm from './pages/Fornecedor/FornecedorForm'
 import ProdutoList from './pages/Produto/ProdutoList'
 import ProdutoForm from './pages/Produto/ProdutoForm'
 import Inicial from './pages/Inicial'
+import ClienteList from './pages/Cliente/ClienteList'
+import ClienteForm from './pages/Cliente/ClienteForm'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <div className='container'>
-        <Routes>
-          <Route path="/" element={<Inicial />} />
-          <Route path="/listar-fornecedores" element={<FornecedorList />} />
-          <Route path="/add-fornecedores" element={<FornecedorForm />} />
-          <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
-          <Route path="/listar-produtos" element={<ProdutoList />} />
-          <Route path="/add-produtos" element={<ProdutoForm />} /> 
-          <Route path="/edit-produtos/:id" element={<ProdutoForm />} /> 
-        </Routes>
-      </div>
+    <Navbar />
+    <div className="container">
+    <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/listar-fornecedores" element={<FornecedorList />} />
+        <Route path="/add-fornecedores" element={<FornecedorForm />} />
+        <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
+        <Route path="/listar-produtos" element={<ProdutoList />} />
+        <Route path="/add-produtos" element={<ProdutoForm />} />
+        <Route path="/add-clientes" element={<ClienteForm />} />
+        <Route path="/listar-clientes" element={<ClienteList />} />
+        <Route path="/edit-clientes/:id" element={<ClienteForm />} />
+      </Routes>
+    </div>
     </BrowserRouter>
   )
 }
